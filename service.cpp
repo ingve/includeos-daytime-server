e9e418f57508fb06aeb3dadaad5a2d862cdbc9d4
Ingve Vormestrand <ingvev@gmail.com>
2017-08-18 15:31:44 +0200

    Updated to recent IncludeOS

--- service.cpp
+++ service.cpp
@@ -12,26 +12,22 @@ std::string make_daytime_string() {
 void Service::start() {
     using namespace std::chrono_literals;
 
-    static auto inet = net::new_ipv4_stack<>({ 10,0,0,42 }, { 255,255,255,0 }, { 10,0,0,1 });
-    auto& server = inet->tcp().bind(port);
+    auto& inet = net::Inet4::ifconfig(5.0, [](bool timeout) {
+      if (timeout) {
+        printf("DHCP TIMEOUT :(\n");
+      }
+    });
 
-    server.onAccept([] (auto conn) -> bool {
-                return true; // allow all connections
-            })
-            .onConnect([] (auto conn) {
-                std::string response = make_daytime_string();
-                conn->write(response.data(), response.size());
-                conn->close();
-            })
-            .onDisconnect([](auto conn, auto reason) {
-                conn->close();
-            })
-            .onError([](auto, auto err) {
-                printf("<Service> @onError - %s\n", err.what());
-            });
+    auto& server = inet.tcp().listen(port);
 
-    hw::PIT::instance().on_repeated_timeout(30s, [] {
-        printf("<Service> TCP STATUS:\n%s\n", inet->tcp().status().c_str());
+    server.on_connect([] (auto conn) {
+      std::string response = make_daytime_string();
+      conn->write(response.data(), response.size());
+      conn->close();
+    });
+
+    Timers::periodic(5s, 30s, [&inet] (uint32_t) {
+      printf("<Service> TCP STATUS:\n%s\n", inet.tcp().status().c_str());
     });
 
     printf("*** DAYTIME SERVICE STARTED ***\n");
